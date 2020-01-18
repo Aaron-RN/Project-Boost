9ef20b6e9b319a927d518c743268bb39427a04cf
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-18 12:10:44 -0500

    added scripts for Mobile touchscreen controls and GUI using Joystick Pack asset from Unity Assets store. Added GUI Canvas Object holding a Button Element for Thrusting. Thrusting button uses a script referencing IPointersHandlers for OnPointer methods.

---
+++ Assets/BtnThrust.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BtnThrust : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool isPressed = false;
+
+    public bool IsPressed()
+    {
+        return isPressed;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+}
---
+++ Assets/Rocket_Android.cs
@@ -0,0 +1,156 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Rocket_Android : MonoBehaviour
+{
+    [SerializeField] bool debugMode = false;
+    [SerializeField] float thrustPower = 950;
+    [SerializeField] float rotateSpeed = 175f;
+    [SerializeField] AudioClip sfxThrust;
+    [SerializeField] AudioClip sfxDeath;
+    [SerializeField] AudioClip sfxWin;
+
+    [SerializeField] ParticleSystem fxThrust;
+    [SerializeField] ParticleSystem fxDeath;
+    [SerializeField] ParticleSystem fxWin;
+    static int sceneLevel = 0; //Level 1
+    static int sceneMaxlevel;
+    float levelLoadDelay = 3f;
+
+    enum State { Alive, Dying, Transcending };
+    State playerState = State.Alive;
+
+    [SerializeField] Joystick joystick;
+    BtnThrust btnThrust;
+    Rigidbody rigidBody;
+    AudioSource audioController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneMaxlevel = SceneManager.sceneCountInBuildSettings - 1;
+
+        rigidBody = GetComponent<Rigidbody>();
+        audioController = GetComponent<AudioSource>();
+        btnThrust = GameObject.Find("BtnThrust").GetComponent<BtnThrust>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.autorotateToLandscapeRight = false;
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        if (playerState == State.Alive)
+        {
+            PlayerThrust();
+            PlayerRotate();
+        }
+
+        if (debugMode)
+        {
+            if (Input.GetKey(KeyCode.L)) { Won(); }
+        }
+    }
+
+    private void ReloadScene()
+    {
+        fxDeath.Stop();
+        SceneManager.LoadScene(sceneLevel);
+    }
+    private void NextScene()
+    {
+        fxWin.Stop();
+        if (sceneLevel < sceneMaxlevel) { sceneLevel++; }
+        else { sceneLevel = 0; }
+        SceneManager.LoadScene(sceneLevel);
+    }
+
+    private void Death()
+    {
+        playerState = State.Dying;
+        audioController.Stop();
+        audioController.PlayOneShot(sfxDeath);
+        Handheld.Vibrate();
+        fxDeath.Play();
+        Invoke("ReloadScene", levelLoadDelay);
+    }
+    private void Won()
+    {
+        if (playerState == State.Transcending) { return; }
+        playerState = State.Transcending;
+        audioController.Stop();
+        audioController.PlayOneShot(sfxWin);
+        fxWin.Play();
+        Invoke("NextScene", levelLoadDelay);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (playerState != State.Alive || debugMode) { return; } //ignore collisions when dead
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+
+            case "Goal":
+                Won();
+                break;
+
+            default:
+                fxThrust.Stop();
+                Death();
+                break;
+        }
+    }
+
+    private void PlayerThrust()
+    {
+        if (btnThrust.IsPressed())
+        {
+            Thrust();
+            fxThrust.Play();
+        }
+        else
+        {
+            audioController.Stop();
+            fxThrust.Stop();
+        }
+    }
+
+    private void Thrust()
+    {
+        float frameThrustPower = thrustPower * Time.deltaTime;
+
+        rigidBody.AddRelativeForce(Vector3.up * frameThrustPower);
+        if (!audioController.isPlaying)
+        {
+            audioController.PlayOneShot(sfxThrust);
+        }
+    }
+
+    private void PlayerRotate()
+    {
+        if (joystick.Horizontal < 0)
+        {
+            RotateDirection(rotateSpeed * Time.deltaTime);
+        }
+        else if (joystick.Horizontal > 0)
+        {
+            RotateDirection(-rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void RotateDirection(float frameRotateSpeed)
+    {
+        //stop natural phystics control and take control of rotation
+        rigidBody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * frameRotateSpeed);
+        //resume natural phystics control
+        rigidBody.freezeRotation = false;
+    }
+}
